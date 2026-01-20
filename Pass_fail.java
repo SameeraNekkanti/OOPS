b8349748904b6e71a49f4ff3fbc4038524660673
SameeraNekkanti <sami.nekkanti@gmail.com>
2026-01-20 16:26:55 +0530

    Add files via upload

---
+++ Pass_fail.java
@@ -0,0 +1,15 @@
+package hello_world;
+
+public class Pass_fail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int mark=50;
+		System.out.println("the mark is:"+mark);
+		if(mark<=49) {
+			System.out.println("fail");
+		} else {
+			System.out.print("pass");
+		}
+	}
+}

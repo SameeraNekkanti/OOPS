9a89f6f669c5a24a9aa666a26ed1d01f5839ebbb
SameeraNekkanti <sami.nekkanti@gmail.com>
2026-01-24 22:29:06 +0530

    Add files via upload

---
+++ SumOfTenNumbers.java
@@ -0,0 +1,19 @@
+package hello_world;
+
+public class SumOfTenNumbers {
+
+	public static void main(String[] args) {
+		int count=0;
+		for(int i=1;i<=10;i++) {
+			System.out.println(i);
+			count+=i;
+			
+			
+		}
+		System.out.println();
+		System.out.println("the sum is:"+count);
+		// TODO Auto-generated method stub
+
+	}
+
+}
